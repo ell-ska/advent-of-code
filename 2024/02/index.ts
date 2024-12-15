a3262ec18085d9dd1019d54be3ab767f6dac41cc
ell-ska <ella@skagerstrom.se>
2024-12-15 22:10:54 +0100

    improve readability in 2024/02

--- 2024/02/index.ts
+++ 2024/02/index.ts
@@ -3,50 +3,38 @@ import fs from 'fs'
 const minDifference = 1
 const maxDifference = 3
 
+const getDifference = (current: number, next: number) => current - next
+
+const validateReport = (levels: number[], isIncreasing: boolean) => {
+  return levels.every((_, index) => {
+    // last level, no next step
+    if (index >= levels.length - 1) {
+      return true
+    }
+
+    const difference = getDifference(levels[index], levels[index + 1])
+
+    return isIncreasing
+      ? difference <= -minDifference && difference >= -maxDifference
+      : difference >= minDifference && difference <= maxDifference
+  })
+}
+
 const part1 = (document: string) => {
   const reports = document.split('\n')
 
-  const safes = reports.map((report) => {
-    const levels = report.split(' ')
+  return reports.reduce((safeReportCount, report) => {
+    const levels = report.split(' ').map(Number)
 
-    const difference = parseInt(levels[0]) - parseInt(levels[1])
-    if (difference === 0) return false
+    const difference = getDifference(levels[0], levels[1])
+    // if there is no difference between the first to levels the report is invalid
+    if (difference === 0) return safeReportCount
 
-    const isIncreasing = difference <= minDifference * -1
+    const isIncreasing = difference < 0
+    const isSafeReport = validateReport(levels, isIncreasing)
 
-    const validSteps = levels.map((level, index) => {
-      if (index >= levels.length - 1) {
-        return true
-      }
-
-      const difference = parseInt(level) - parseInt(levels[index + 1])
-
-      if (
-        isIncreasing &&
-        difference <= minDifference * -1 &&
-        difference >= maxDifference * -1
-      ) {
-        return true
-      } else if (
-        !isIncreasing &&
-        difference >= minDifference &&
-        difference <= maxDifference
-      ) {
-        return true
-      } else {
-        return false
-      }
-    })
-
-    return validSteps.every((step) => step === true)
-  })
-
-  const sum = safes.reduce(
-    (previous, safe) => (safe === true ? previous + 1 : previous),
-    0
-  )
-
-  return sum
+    return isSafeReport ? safeReportCount + 1 : safeReportCount
+  }, 0)
 }
 
 const document = fs.readFileSync('2024/02/input.txt', 'utf-8')
